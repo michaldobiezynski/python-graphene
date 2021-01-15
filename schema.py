864b6982266390dcd00c0f22a65724e12ea23db2
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-15 14:29:29 +0000

    object types/arguments in queries

--- schema.py
+++ schema.py
@@ -1,7 +1,14 @@
 import graphene
 import json
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
 
 class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
     is_admin = graphene.Boolean()
 
@@ -11,12 +18,22 @@ class Query(graphene.ObjectType):
     def resolve_is_admin(self, info):
         return True
 
-schema = graphene.Schema(query=Query, auto_camelcase=False)
+    def resolve_users(self, info, limit=None):
+        return [
+            User(id="1", username="Fred", created_at=datetime.now()),
+            User(id="2", username="Doug", created_at=datetime.now())
+        ][:limit]
+
+schema = graphene.Schema(query=Query)
 
 result = schema.execute(
     '''
     {
-        is_admin
+        users(limit: 1) {
+            id
+            username
+            createdAt
+        }
     }    
     '''
 )
